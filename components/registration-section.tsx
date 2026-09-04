a0fd30ef34e06b3ce93feb21bae2e46e2bf334db
Skjumu <jumusk691@gmail.com>
2026-09-04 23:22:24 +0530

    Update APK download link to Google Drive

--- components/registration-section.tsx
+++ components/registration-section.tsx
@@ -6,7 +6,7 @@ const steps = [
     icon: Download,
     title: 'Download APK',
     text: 'Click download and install the MYT app.',
-    href: 'https://www.mediafire.com/file/xz412xcsbc6ugff/MYT🇮🇳.apk/file', 
+    href: 'https://drive.google.com/file/d/1j0wTIxvYdzE_mTOpUn2e7LmnilC9uWeW/view?usp=drivesdk', 
   },
   {
     icon: Smartphone,
